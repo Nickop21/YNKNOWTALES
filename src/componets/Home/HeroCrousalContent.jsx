7d0805660746385b80645cdf5344be968da33ce0
Nickop21 <ny8244@gmail.com>
2024-06-13 13:03:35 +0530

    added crousal in home section

---
+++ src/componets/Home/HeroCrousalContent.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Button, Carousel, Typography } from "@material-tailwind/react";
+
+
+const imgArray = [
+  "https://images.unsplash.com/photo-1488372759477-a7f4aa078cb6?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://plus.unsplash.com/premium_photo-1682513184135-b7b9b76fb4eb?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1531845116688-48819b3b68d9?q=80&w=2942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1642715614665-8e5534e7e427?q=80&w=2486&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  " https://images.unsplash.com/photo-1623837922074-8d29ce30c40f?q=80&w=2865&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+];
+function HeroCrousalContent() {
+  return (
+
+    <Carousel
+    loop={true}
+    autoplay={true}
+    transition={{ duration: 1 }}
+    className="rounded-xl"
+  >
+    {imgArray.map((imgData,index) => (
+      // console.log(imgData)
+      <img
+      key={index}
+        className="h-[100%] w-full object-cover object-center rounded-lg"
+        src={imgData}
+        alt={`nature image ${index}`}
+      />
+    ))}
+  </Carousel>
+  )
+}
+
+export default HeroCrousalContent
--- src/componets/Home/HeroSection.jsx
+++ src/componets/Home/HeroSection.jsx
@@ -1,42 +1,40 @@
 import React from "react";
 import Container from "../Container";
-import { Button, Typography } from "@material-tailwind/react";
+import { Button, Carousel, Typography } from "@material-tailwind/react";
 import CreateSection from "./CreateSection";
+import HeroCrousalContent from "./HeroCrousalContent";
+
 
 function HeroSection() {
   return (
-    <main className="h-[500px] relatives">
+    <main className="h-[500px] relative">
       <section className="h-[100%] flex ">
         <div className="w-[50%] h-[100%] px-2 bg-[#121316] p-[20px]">
           <div className="p-10 pt-[100px]">
             <Typography variant="h6" color="yellow" className="mb-2">
-              Newest Blog
+              Welcome Narrator
             </Typography>
-            <Typography variant="h1" color="white" className="mb-2">
-              The art of Home Transform
+            <Typography variant="h2" color="white" className="mb-2">
+              Empower Others by Sharing Your Story
             </Typography>
             <p className="mb-2 text-[grey]">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
-              aliquid est, facere vel veniam nostrum eum qui possimus sapiente
-              vero deleniti nulla, quibusdam voluptate inventore at iusto
-              magnam! Veritatis, eveniet.
+              Your story is a treasure trove waiting to inspire, enlighten, and
+              connect with others. Share it boldly, for in your experiences lie
+              the seeds of growth and the power to uplift countless souls
             </p>
-            <Button color="amber" className="mt-5 ">
-              Read more
-            </Button>
+            {/* <Button color="amber" className="mt-5 ">
+          Read more
+        </Button> */}
           </div>
         </div>
 
         {/* left section */}
         <div className="w-[50%] px-2 h-[100%] ">
-          <img
-            className="h-[100%] w-full object-cover object-center rounded-lg"
-            src="https://images.unsplash.com/photo-1682407186023-12c70a4a35e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80"
-            alt="nature image"
-          />
+          <HeroCrousalContent/>
         </div>
       </section>
-      <CreateSection/>
+
+      <CreateSection />
     </main>
   );
 }
