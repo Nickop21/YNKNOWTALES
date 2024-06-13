0dd9b2ddc86c8d958f4a65e2454d648b21fadf89
Nickop21 <ny8244@gmail.com>
2024-06-13 13:04:49 +0530

    fixed alignment issue in create tab

--- src/componets/Home/CreateSection.jsx
+++ src/componets/Home/CreateSection.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from 'react-router-dom';
 export default function CreateSection() {
   return (
     
-          <div className="h-[60px] w-full max-w-2xl mx-auto px-6 absolute  bg-white z-50 flex flex-row items-center justify-between rounded-lg inset-x-0 bottom-[260px] mx-auto ">
+          <div className="h-[60px] w-full max-w-2xl mx-auto px-6 absolute  bg-white z-50 flex flex-row items-center justify-between rounded-lg inset-x-0 -bottom-[20px] mx-auto ">
         <h6 className="font-extrabold text-lg " >
             Share your stories with us
             </h6>
