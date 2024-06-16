1512cb814973dc9e05897c19857154dabf991cf3
Nickop21 <ny8244@gmail.com>
2024-06-17 01:01:24 +0530

    cleanup + hide routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,59 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
-import './App.css'
+import "./App.css";
 
-import { useDispatch } from 'react-redux';
-import authService from "./appwrite/auth"
-import appwriteService from "./appwrite/config.js"
-import {login, logout} from "./store/authSlice"
-import {AllPost} from "./store/postSlice.js"
-import { Outlet } from 'react-router-dom'
-import Login from "./pages/Login.jsx";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import appwriteService from "./appwrite/config.js";
+import { login, logout } from "./store/authSlice";
+import { AllPost } from "./store/postSlice.js";
+import { Outlet, useLocation } from "react-router-dom";
+import Header from "./componets/Header.jsx";
+import Footer from "./componets/Footer.jsx";
+import Loader from "./componets/Loader.jsx";
 function App() {
-  
-  const [loading, setLoading] = useState(true)
-  const dispatch = useDispatch()
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const hideHeaderRoutes = ["/login", "/signup"];
 
   useEffect(() => {
-    authService.getCurrentUser()
-    .then((userData) => {
-      if (userData) {
-        dispatch(login({userData}))
-      } else {
-        dispatch(logout())
-      }
-    })
-    .finally(() => setLoading(false))
-
-    appwriteService.getPosts().then((PostsData)=>{
-      setLoading(true)
-      
-      if(PostsData){
-        dispatch(AllPost(PostsData.documents))
-      }
-    }).finally(() => setLoading(false))
-
-
-
-  }, [])
-
-  
+    if (hideHeaderRoutes.includes(location.pathname)) {
+      setLoading(false);
+      return;
+    }
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+          appwriteService.getPosts().then((PostsData) => {
+            if (PostsData) {
+              dispatch(AllPost(PostsData.documents));
+            }
+          });
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
 
   return (
     <>
-
-     <main>
-    {/* <Login/> */}
-      {!loading &&<Outlet/>
-      }
- 
+      {!loading ? (
+        <>
+          {!hideHeaderRoutes.includes(location.pathname) && <Header />}
+        <main>
+          <Outlet />
         </main>
-     
+          {!hideHeaderRoutes.includes(location.pathname) && <Footer />}
+        </>
+      ) : (
+        <Loader />
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
