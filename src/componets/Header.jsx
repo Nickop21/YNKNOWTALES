a7fe49b293df056855c2977a014c4b4a1022eb04
Nickop21 <ny8244@gmail.com>
2024-06-16 12:05:34 +0530

    added navigate  to btns in header

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import { Avatar, Button, Typography } from "@material-tailwind/react";
 import logouticon from "../img/logout.png"
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { logout } from "../store/authSlice";
+import authservice from "../appwrite/auth";
 
 
 function Header() {
+  const navigate=useNavigate()
+  const dispatch=useDispatch()
+
+  function logouthandler() {
+    dispatch(logout())
+    authservice.deleteSession()
+    
+  }
   return (
     < >
         <div className="h-[65px] w-full max-w-7xl mx-auto px-6 fixed   bg-white shadow-md z-[99999] flex flex-row items-center justify-between rounded-b-lg inset-x-0 top-0 mx-auto ">
-          <div className="flex gap-3 ml-9">
+          <div className="flex gap-3 ml-9 cursor-pointer" onClick={()=>navigate("/")}>
             {/* <img className="h-[20px] w-[20px] " src="" alt="YNKOWTALES" /> */}
             <h6 className="font-extrabold text-lg" >
             YNKOW <span className="text-amber-500 font-extrabold text-xl">Tales</span>
@@ -16,7 +28,7 @@ function Header() {
           </div>
 
           <div className="flex gap-3 mr-2">
-          <Button color="amber" className=" text-black">
+          <Button color="amber" className=" text-black" onClick={()=>logouthandler()}>
              Logout
             </Button>
             {/* <Avatar src={logouticon} alt="logout" /> */}
