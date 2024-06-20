65e2435b0a53c10c6d32cb4422e891f4cc835b4e
Nickop21 <ny8244@gmail.com>
2024-06-20 18:11:42 +0530

    changes and error showing in tiny react

--- src/componets/RTE.jsx
+++ src/componets/RTE.jsx
@@ -1,27 +1,33 @@
-import React from 'react'
-import {Editor } from '@tinymce/tinymce-react';
-import {Controller } from 'react-hook-form';
-import conf from '../conf/conf';
+import React from "react";
+import { Editor } from "@tinymce/tinymce-react";
+import { Controller } from "react-hook-form";
+import conf from "../conf/conf";
 
-
-export default function RTE({name, control, label, defaultValue =""}) {
+export default function RTE({
+  name,
+  control,
+  label,
+  defaultValue = "",
+  error,
+}) {
   return (
-    <div className='w-full z-50'> 
-    {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
+    <div className="w-full z-50 mt-5">
+      {label && <label className="inline-block mb-1 pl-1">{label}</label>}
 
-    <Controller
-    name={name || "content"}
-    control={control}
-    render={({field: {onChange}}) => (
-        <Editor
-        apiKey={conf.tinymceid}
-        initialValue={defaultValue}
-        init={{
-            initialValue: defaultValue,
-            height: 500,
-            menubar: true,
-            
-            plugins: [
+      <Controller
+        name={name || "content"}
+        control={control}
+        rules={{ required: "Content is required" }}
+        render={({ field: { onChange } }) => (
+          <Editor
+            apiKey={conf.tinymceid}
+            initialValue={defaultValue}
+            init={{
+              initialValue: defaultValue,
+              height: 500,
+              menubar: true,
+
+              plugins: [
                 "image",
                 "advlist",
                 "autolink",
@@ -42,16 +48,19 @@ export default function RTE({name, control, label, defaultValue =""}) {
                 "help",
                 "wordcount",
                 "anchor",
-            ],
-            toolbar:
-            "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
-            content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
-        }}
-        onEditorChange={onChange}
-        />
-    )}
-    />
-
-     </div>
-  )
+              ],
+              toolbar:
+                "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
+              content_style:
+                "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+            }}
+            onEditorChange={onChange}
+          />
+        )}
+      />
+      {error && (
+        <p className="text-red-700 text-sm mt-2">Content required to post</p>
+      )}
+    </div>
+  );
 }
