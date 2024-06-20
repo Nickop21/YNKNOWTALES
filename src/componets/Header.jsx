05578db0a98edfeb50687fd0e8e57d9553e91518
Nickop21 <ny8244@gmail.com>
2024-06-20 18:11:23 +0530

    adeed delete session on logout

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -6,47 +6,50 @@ import { logout } from "../store/authSlice";
 import authservice from "../appwrite/auth";
 import Loader from "./Loader";
 
-
 function Header() {
-  const navigate=useNavigate()
-  const dispatch=useDispatch()
-  const [loading, setloading] = useState(false)
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [loading, setloading] = useState(false);
 
   function logouthandler() {
-    authservice.deleteSession()
-    setloading(true)
+    authservice.deleteSession();
+    setloading(true);
     setTimeout(() => {
-      dispatch(logout())
-      setloading(false)
+      dispatch(logout());
+      setloading(false);
     }, 500);
-    
   }
   return (
-    < >
-        <div className="h-[65px] w-full max-w-7xl mx-auto px-6 fixed   bg-white shadow-md z-[99999] flex flex-row items-center justify-between rounded-b-lg inset-x-0 top-0 mx-auto ">
-          
-          <div className="flex gap-3 ml-9 cursor-pointer" onClick={()=>navigate('/')}>
-            {/* <img className="h-[20px] w-[20px] " src="" alt="YNKOWTALES" /> */}
-            <h6 className="font-extrabold text-lg" >
-            YNKOW <span className="text-amber-500 font-extrabold text-xl">Tales</span>
-            </h6>
-           
-          </div>
-      
-          
+    <>
 
-          <div className="flex gap-3 mr-2">
-          <Button color="amber" className=" text-black" onClick={()=>logouthandler()}>
-             Logout
-            </Button>
-    
-            <Avatar
-              src="https://docs.material-tailwind.com/img/face-2.jpg"
-              alt="avatar"
-            />
-          </div>
+      <div className="h-[65px] w-full max-w-7xl mx-auto px-6 fixed   bg-white shadow-md z-[99999] flex flex-row items-center justify-between rounded-b-lg inset-x-0 top-0 mx-auto ">
+        <div
+          className="flex gap-3 ml-9 cursor-pointer"
+          onClick={() => navigate("/")}
+        >
+          {/* <img className="h-[20px] w-[20px] " src="" alt="YNKOWTALES" /> */}
+          <h6 className="font-extrabold text-lg">
+            YNKOW{" "}
+            <span className="text-amber-500 font-extrabold text-xl">Tales</span>
+          </h6>
         </div>
-        {loading && <Loader/>}
+
+        <div className="flex gap-3 mr-2">
+          <Button
+            color="amber"
+            className=" text-black"
+            onClick={() => logouthandler()}
+          >
+            Logout
+          </Button>
+
+          <Avatar
+            src="https://docs.material-tailwind.com/img/face-2.jpg"
+            alt="avatar"
+          />
+        </div>
+      </div>
+      {loading && <Loader />}
     </>
   );
 }
